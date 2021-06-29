cdff3966d82fef09b790d129d0370f1441227a5e
Dan Rasmussen <dan.rasmussen@sumup.com>
2021-06-29 08:12:17 +0000

    SageMaker PyTorch model server

--- src/model.py
+++ src/model.py
@@ -3,8 +3,11 @@ This tutorial borrows from https://huggingface.co/transformers/custom_datasets.h
 """
 import argparse
 import os
+import sys
+import logging
 
 import boto3
+import json
 import numpy as np
 import pandas as pd
 from sklearn.metrics import precision_score, recall_score, average_precision_score
@@ -12,9 +15,10 @@ from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader, RandomSampler
 from transformers import AdamW, AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
-
-from utils import read_object
-
+        
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 class AmazonReviewDataset(Dataset):
     """Inherits from torch.utils.data.Dataset.
@@ -77,21 +81,21 @@ def compute_metrics(output):
         "recall": recall,
         "auprc": auprc
     }
-
-
+    
 def main(args):
     """Executes training job."""
     # Load data into a pandas DataFrame.
     df = read_object(args.input_path, file_type="json").sample(frac=1.)
     if args.max_data_rows:
         df = df.iloc[:args.max_data_rows].copy()
+        
     print(f"Data contains {len(df)} rows")
     # Preprocess and featurize data.
     traindf, validdf = preprocess_data(df)
     # Create data set for model input.
     train_dataset = AmazonReviewDataset(traindf, args.model_name, args.max_sequence_length)
     valid_dataset = AmazonReviewDataset(validdf, args.model_name, args.max_sequence_length)
-    print(train_dataset[0])
+
     # Define model and trainer.
     model = AutoModelForSequenceClassification.from_pretrained(
         args.model_name,
@@ -122,22 +126,85 @@ def main(args):
     # Train the model.
     trainer.train()
     trainer.evaluate()
-
-    # Elastic Inference
+    
     # Save the model to the output_path defined in train_model.py.
-    device = torch.device("cuda")
-    dummy_row = train_dataset[0]
+    save_model(model, args.model_dir)
+    
+def read_object(s3_path, file_type, **kwargs):
+    """Read a file from S3 into a pandas.DataFrame.
+
+    Arguments:
+        s3_path (str)
+
+    Returns:
+        df (pd.DataFrame)
+    """
+    # Get S3 client.
+    s3 = boto3.client('s3')
+    # Get object.
+    bucket, key = s3_path.split('/')[2], '/'.join(s3_path.split('/')[3:])
+    object = s3.get_object(Bucket=bucket, Key=key)
+    # Read object into pandas.DataFrame.
+    if file_type == "csv":
+        df = pd.read_csv(object['Body'], **kwargs)
+    elif file_type == "json":
+        df = pd.read_json(object['Body'], lines=True, **kwargs)
+    else:
+        raise Exception("Only file_type csv and json currently supported.")
+    return df
+
+
+def write_object(df, s3_path, file_type="json"):
+    """Save a pandas.DataFrame to S3.
+
+    Arguments:
+        df (pd.DataFrame)
+        s3_path (str)
+    """
+    if file_type == "csv":
+        df.to_csv(s3_path)
+    elif file_type == "json":
+        df.to_json(s3_path, lines=True, orient="records")
+    else:
+        raise Exception("Only file_type csv and json currently supported.")
+
+        
+def save_model(model, model_dir):
+    logger.info(f"Save model...")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+    dummy_row = encode_sequences(["dummy"])
     dummy_input = (
-        dummy_row["input_ids"].unsqueeze(0).to(device),
-        dummy_row["attention_mask"].unsqueeze(0).to(device)
+        dummy_row["input_ids"][0].unsqueeze(0).to(device),
+        dummy_row["attention_mask"][0].unsqueeze(0).to(device)
     )
-    traced_model = torch.jit.trace(model, dummy_input)
-    torch.jit.save(traced_model, os.path.join(args.model_dir, "model.pth"))
+    traced_model = torch.jit.trace(model.to(device), dummy_input)
+    
+    model_path = os.path.join(model_dir, "model.pth")
+    logger.info(f"Model path: {model_path}")
+    
+    torch.jit.save(traced_model, model_path)
+    
+    
+tokenizer = None
 
-    # Save the model to the output_path defined in train_model.py.
-    with open(os.path.join(args.model_dir, 'model.pth'), 'wb') as f:
-        torch.save(model.state_dict(), f)
+def get_tokenizer(name):
+    global tokenizer
+    if tokenizer == None:
+        tokenizer = AutoTokenizer.from_pretrained(name)
+        
+    return tokenizer
 
+def encode_sequences(sequences):
+    tokenizer = get_tokenizer("distilbert-base-uncased")
+
+    return tokenizer.batch_encode_plus(
+        sequences,
+        max_length=300,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt"
+    )
 
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()
@@ -162,3 +229,53 @@ if __name__ =='__main__':
     # Parse command-line args and run main.
     args = parser.parse_args()
     main(args)
+
+##################################
+# SageMaker PyTorch model server
+
+VERSIONS_USE_NEW_API = ["1.5.1"]
+
+    
+def model_fn(model_dir):
+    logger.info(f"model_fn")
+    try:
+        model_path = os.path.join(model_dir, "model.pth")
+        
+        return torch.jit.load(model_path, map_location=torch.device("cpu"))
+    
+    except Exception as e:
+        logger.exception(f"Exception in model fn {e}")
+        return None
+      
+        
+def input_fn(request_body, request_content_type):
+    logger.info(f"input_fn")
+    logger.info(f"request_content_type: {request_content_type}")
+    logger.info(request_body)
+    
+    if request_content_type == "application/json":
+        sequences = json.loads(request_body)
+        logger.info(sequences)
+        
+        return encode_sequences(sequences)
+
+    else:
+        logger.exception(f"Unhandled content type in input fn: {request_content_type}")
+        raise
+
+
+def predict_fn(input_data, model):
+    logger.info(f"predict_fn")
+
+    device = torch.device("cpu")
+    model.to(device)
+    model.eval()
+
+    input_ids = input_data['input_ids']
+    attention_mask = input_data['attention_mask']
+    
+    input_ids.to(device)
+    attention_mask.to(device)
+    
+    with torch.no_grad():
+        return model(input_ids, attention_mask)
